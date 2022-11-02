43c8dbe543baf38b34a34b1a6fff42f015f62b24
Mark Buensalido <mbuensalido@mac.com>
2022-11-02 10:46:02 -0700

    Added creds and changed version

--- main.tf
+++ main.tf
@@ -1,20 +1,45 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = "3.5.0"
+      source  = "hashicorp/google"
+      version = "4.41.0"
     }
   }
 }
 
 provider "google" {
-  credentials = file("<NAME>.json")
+  credentials = file("hc-fd16295afe104a7d887d3581496-4ad48ad79f5c.json")
 
-  project = "<PROJECT_ID>"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = var.gcp_project
+  region  = var.gcp_region
+  zone    = var.gcp_az
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+resource "google_sql_database" "database" {
+  name     = "mark-test-db"
+  instance = google_sql_database_instance.instance.name
 }
+
+# found this code on the GCP site and looks pretty good
+resource "google_sql_database_instance" "postgres_instance" {
+  name             = "mark-test-db-instance"
+  region           = var.region
+  database_version = "POSTGRES_14"
+  root_password    = "Passw0rd1!"
+  settings {
+
+    # want to specify a really small tier
+    tier = "db-f1-micro"
+
+    ip_configuration {
+      ipv4_enabled = true
+    }
+
+  }
+  deletion_protection = false # set to true to prevent destruction of the resource
+}
+
+
+# resource "google_compute_network" "vpc_network" {
+#   name = "terraform-network"
+# }
--- variables.auto.tfvars
+++ variables.auto.tfvars
@@ -1,3 +1,19 @@
-variable "gcpregion" {
-  description = "The region that this resource will be provisioned in."
+variable "gcp_project" {
+  description = "The GCP Project"
+  type        = string
+}
+
+variable "gcp_region" {
+  description = "The GCP region that this resource will be provisioned in."
+  type        = string
+}
+
+variable "gcp_az" {
+  description = "The GCP AZ region that this resouce will be provisioned in."
+  type        = string
+}
+
+output "gcp_cloudsql_dbname" {
+  description = "This is the name of the database that gets created."
+  type        = string
 }
